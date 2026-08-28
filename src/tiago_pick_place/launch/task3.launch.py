e5896c17946d62571835fa06681a5806bee6ef0f
InesHiggs <ismesquita@gmail.com>
2026-08-28 16:53:24 +0200

    Added localization server, Nav2, AMCL, MAP

--- src/tiago_pick_place/launch/task3.launch.py
+++ src/tiago_pick_place/launch/task3.launch.py
@@ -3,7 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
@@ -31,8 +31,78 @@ def generate_launch_description():
         ),
         launch_arguments={
             'world_name': 'group10',
+            'moveit': 'True',
             'use_sim_time': 'True',
         }.items(),
+    )
+
+    # ----------------------------------------------------------
+    # Navigation stack: known map + AMCL + Nav2 + RViz
+    # Adapted directly from our working Task 2
+    # ----------------------------------------------------------
+
+    navigation = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('tiago_2dnav'),
+                'launch',
+                'tiago_nav_bringup.launch.py',
+            )
+        ),
+        launch_arguments={
+            'map_path': '/home/ines/tiago_ws/group10_map',
+            'use_sim_time': 'True',
+            'is_public_sim': 'false',
+            'rviz': 'True',
+            'slam': 'False',
+        }.items(),
+    )
+
+    localization = Node(
+        package='tiago_autonomous_navigation',
+        executable='localization_server',
+        name='localization_server',
+        output='screen',
+        parameters=[{
+            'use_sim_time': True,
+        }],
+    )
+
+    aruco_pick = Node(
+        package='aruco_ros',
+        executable='single',
+        name='aruco_single_26',
+        output='screen',
+
+        parameters=[{
+            'image_is_rectified': True,
+
+            # PICK-location marker from Task 2
+            'marker_id': 26,
+            'marker_size': 0.25,
+
+            'camera_frame': CAMERA_FRAME,
+            'marker_frame': 'aruco_marker_26',
+            'reference_frame': '',
+
+            'corner_refinement': 'LINES',
+            'use_sim_time': True,
+        }],
+
+        remappings=[
+            ('/image', CAMERA_IMAGE_TOPIC),
+            ('/camera_info', CAMERA_INFO_TOPIC),
+        ],
+    )
+
+    target_pose_server = Node(
+        package='tiago_autonomous_navigation',
+        executable='target_pose_server',
+        name='target_pose_server',
+        output='screen',
+        parameters=[{
+            'use_sim_time': True,
+        }],
     )
 
     # ----------------------------------------------------------
@@ -108,9 +178,50 @@ def generate_launch_description():
         parameters=[{'use_sim_time': True}],
     )
 
-    return LaunchDescription([
-        tiago_world,
-        aruco_63,
-        aruco_582,
-        cube_tracker,
-    ])
+    camera_tf_alias = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='camera_frame_alias',
+        arguments=[
+            '0', '0', '0',
+            '0', '0', '0',
+            'head_front_camera_color_optical_frame',
+            'head_front_camera_rgb_optical_frame',
+        ],
+        parameters=[{'use_sim_time': True}],
+        output='screen',
+    )
+
+    ld = LaunchDescription()
+
+    # Start Gazebo/TIAGo immediately
+    ld.add_action(tiago_world)
+
+    # Camera-frame alias can also start immediately
+    ld.add_action(camera_tf_alias)
+
+    # Give Gazebo some time, then start Nav2 + RViz
+    ld.add_action(
+        TimerAction(
+            period=2.5,
+            actions=[navigation],
+        )
+    )
+
+    # After Nav2 has had time to initialise:
+    # localization + all perception nodes
+    ld.add_action(
+        TimerAction(
+            period=13.0,
+            actions=[
+                localization,
+                target_pose_server,
+                aruco_pick,
+                aruco_63,
+                aruco_582,
+                cube_tracker,
+            ],
+        )
+    )
+
+    return ld
--- src/tiago_pick_place/tiago_pick_place/cube_tracker.py
+++ src/tiago_pick_place/tiago_pick_place/cube_tracker.py
@@ -2,14 +2,25 @@ import rclpy
 
 from rclpy.node import Node
 from geometry_msgs.msg import PoseStamped
+from tf2_ros import Buffer, TransformListener, TransformException
+import tf2_geometry_msgs
+import rclpy.duration
 
 CUBE_1_ID = 63
 CUBE_2_ID = 582
+
+TARGET_FRAME = 'base_footprint'
 
 class CubeTracker(Node):
     def __init__(self):
         super().__init__('cube_tracker')
 
+        self.tf_buffer = Buffer()
+
+        self.tf_listener = TransformListener(
+            self.tf_buffer,
+            self,
+        )
         self.create_subscription(
             PoseStamped,
             '/aruco_single_63/pose',
@@ -29,20 +40,59 @@ class CubeTracker(Node):
         )
 
     def cube_63_callback(self, msg: PoseStamped):
-        self.get_logger().info(
+        self.get_logger().info( #Camera POV
             f'Cube 63 detected! '
             f'x={msg.pose.position.x:.3f}, '
             f'y={msg.pose.position.y:.3f}, '
             f'z={msg.pose.position.z:.3f}'
         )
+        self.transform_to_base(msg, CUBE_1_ID) #Robot POV
 
     def cube_582_callback(self, msg: PoseStamped):
-        self.get_logger().info(
+        self.get_logger().info(#Camera POV
             f'Cube 582 detected! '
             f'x={msg.pose.position.x:.3f}, '
             f'y={msg.pose.position.y:.3f}, '
             f'z={msg.pose.position.z:.3f}'
         )
+        self.transform_to_base(msg, CUBE_2_ID) #Robot POV
+    
+    def transform_to_base(self, msg: PoseStamped, cube_id: int):
+        source_frame = msg.header.frame_id
+
+        try:
+            transform = self.tf_buffer.lookup_transform(
+                TARGET_FRAME,
+                source_frame,
+                msg.header.stamp,
+                timeout=rclpy.duration.Duration(seconds=1.0)
+            )
+
+            pose_base = tf2_geometry_msgs.do_transform_pose_stamped(
+                msg,
+                transform,
+            )
+
+            pose_base.header.frame_id = TARGET_FRAME
+
+            p = pose_base.pose.position
+
+            self.get_logger().info(
+                f'Cube {cube_id} in {TARGET_FRAME}: '
+                f'x={p.x:.3f}, '
+                f'y={p.y:.3f}, '
+                f'z={p.z:.3f}'
+            )
+            return pose_base
+        
+        except TransformException as exc:
+            self.get_logger().warn(
+                f'Could not transform cube {cube_id}: {exc}'
+            )
+
+            return None
+
+
 
 def main(args=None):
     rclpy.init(args=args)
